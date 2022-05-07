7b4b5a270c278238a80c39f797333b82e27a1c79
pashunt13 <pavelkozencev2002@gmail.com>
2022-05-07 12:55:14 +0300

    исправление ошибок

--- pages/api/basketItem/index.tsx
+++ pages/api/basketItem/index.tsx
@@ -3,15 +3,14 @@ import { BasketItem as BasketItemEntity } from 'src/entity/BasketItem';
 import type { NextApiRequest, NextApiResponse } from 'next';
 
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
+  const connection = await prepareConnection();
+  const basketItemRepository = connection.getRepository(BasketItemEntity);
+
   if (req.method == 'GET') {
-    const connection = await prepareConnection();
-    const basketItemRepository = connection.getRepository(BasketItemEntity);
     const basketItems = await basketItemRepository.find();
     res.status(200).json(basketItems);
   } else if (req.method == 'POST') {
     const item = req.body;
-    const connection = await prepareConnection();
-    const basketItemRepository = connection.getRepository(BasketItemEntity);
     const basketItems = await basketItemRepository.save({ item });
     res.status(201).json(basketItems);
   }
--- src/components/Menu/ToCart.tsx
+++ src/components/Menu/ToCart.tsx
@@ -1,6 +1,4 @@
-import { responseSymbol } from 'next/dist/server/web/spec-compliant/fetch-event';
-import { useEffect, useState } from 'react';
-import { useAppContext } from 'src/context/state';
+import { useState } from 'react';
 import { Item } from 'src/models/index';
 import styles from 'styles/menu.module.css';
 
@@ -12,28 +10,27 @@ export default function ToCart({ item }: ToCartProps) {
   const [isClicked, setIsClicked] = useState(false);
   const [buttonClass, setButtonClass] = useState(styles.toCart);
   const [buttonValue, setButtonValue] = useState('В корзину');
-  // const {basketItems, setBasketItems} = useAppContext();
-  const [basketItems, setBasketItems] = useState([]);
 
   const addToCartHandler = async () => {
     if (isClicked) return;
+    try {
+      const response = await fetch('/api/basketItem', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          Accept: 'application/json',
+        },
+        body: JSON.stringify(item.id),
+      });
+      const data = await response.json();
+      console.log(data);
+    } catch (error) {
+      console.log(error);
+    }
 
     setIsClicked(true);
     setButtonClass(styles.inCart);
     setButtonValue('В корзине');
-
-    const response = await fetch('/api/basketItem', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-        Accept: 'application/json',
-      },
-      body: JSON.stringify(item.id),
-    });
-    const data = await response.json();
-    console.log(data);
-
-    // return setBasketItems([...basketItems, item]);
   };
 
   return (
